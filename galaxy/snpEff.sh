61e59fc9d859240dc3d4b43107a17b4b0fe1b41f
Galaxy <ehills666@gmail.com>
2011-12-13 12:20:02 +1300

    added semi working download command for snpeff

--- galaxy/snpEff.sh
+++ galaxy/snpEff.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 #file downloads Gene sequence if it is not already in the SNPEFF data folder
-#
-#$1 udlength
-#$2 filterIn
-#$3 filterHomeHet
-#$4 statsFile
-#$5 genomeVersion
-#$6 input
-#$7 output
-#$8 filterOut
-#$9 config file
+#$1 genomeVersion
+#$2 Config file
+
 
 if [ ! -d "/home/galaxy/tool-data/shared/jars/snpEff/data/$5" ]
 then
-    java -jar ~/galaxy/tool-data/shared/jars/snpEff/snpEff.jar download $5
-    # Suppress output so user isnt bothered
+    java -jar  ~/galaxy-dist/tool-data/shared/jars/snpEff/snpEff.jar download $1 -c $2
 fi
 
-# Put this back in the xml file and just do the script stuff perhaps
-java -Xmx6G -jar ~/galaxy-dist/tool-data/shared/jars/snpEff/snpEff.jar -c $9 -i vcf -o vcf -upDownStreamLen $1 $2 $3  
-     -no $8 -stats $4 $5 $6 > $7 
 
 
