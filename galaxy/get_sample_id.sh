882494312ecec34535716e9a87f4a09e0d30672c
Galaxy <ehills666@gmail.com>
2012-01-09 11:09:26 +1300

    updated select sample ids

--- galaxy/get_sample_id.sh
+++ galaxy/get_sample_id.sh
@@ -1,1 +1,12 @@
-#!/
+#!/bin/bash
+# Get the sample ids out of a given vcf file
+# author: James Boocock
+# date: 09/01/2011
+#
+# $1 Input File to extract the ids from.
+
+OUTPUT=`grep "^#[^#]" -m 1 $1`
+for line in $OUTPUT
+do
+    echo $line
+done
