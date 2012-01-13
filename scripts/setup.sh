71ef3f38f54e5fb6c7b3a76874f8f33aa81c8216
Galaxy <ehills666@gmail.com>
2012-01-13 13:11:26 +1300

    created setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,16 +7,48 @@
 # galaxy installation folder.
 #
 
-echo Shifting files...
+# Start script
+echo Starting install...
 
-cp -f restart_galaxy.sh /home/galaxy/galaxy-dist/
-cp -f start_galaxy.sh /home/galaxy/galaxy-dist/
-cp -f stop_galaxy.sh /home/galaxy/galaxy-dist/
+# Copy handy scripts to root directory
+mv -f restart_galaxy.sh /home/galaxy/galaxy-dist/
+mv -f start_galaxy.sh /home/galaxy/galaxy-dist/
+mv -f stop_galaxy.sh /home/galaxy/galaxy-dist/
 
-cp -f universe_wsgi.ini /home/galaxy/galaxy-dist/
-cp -f universe_wsgi.runner.ini /home/galaxy/galaxy-dist/
-cp -f universe_wsgi.webapp.ini /home/galaxy/galaxy-dist/
+# Copy config files to root directory
+mv -f universe_wsgi.ini /home/galaxy/galaxy-dist/
+mv -f universe_wsgi.runner.ini /home/galaxy/galaxy-dist/
+mv -f universe_wsgi.webapp.ini /home/galaxy/galaxy-dist/
 
-cp -f tool_conf.xml /home/galaxy/galaxy-dist/
+# Setup tool_conf.xml
+mv -f tool_conf.xml /home/galaxy/galaxy-dist/
+
+# Setup snpEff
+mkdir /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
+mv -f ../src/snpEff/SnpSift.jar /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
+mv -f ../src/snpEff/snpEff.jar /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
+mv -f ../src/snpEff/snpEff.config /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
+mv -fr ../src/snpEff/data /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
+mkdir /home/galaxy/galaxy-dist/tools/snpEff
+mv -f ../src/snpEff/snpEff.xml /home/galaxy/galaxy-dist/tools/snpEff/
+
+# Setup VcfTools
+mv -f ../src/vcfperltools /home/galaxy/galaxy-dist/tool-data/shared/
+
+# Setup GATK
+mkdir /home/galaxy/galaxy-dist/tool-data/shared/jars/gatk
+mv -f ../src/gatk/GenomeAnalysisTK.jar /home/galaxy/galaxy-dist/tool-data/shared/jars/
+mv -f ../src/gatk /home/galaxy/galaxy-dist/tools/
+
+# Install dbSNP
+echo "Downloading dbSNP135 (~9Gb).. This may take some time.."
+wget ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/v4.0/00-All.vcf.gz 
+mkdir ../data
+mv -f 00-All.vcf.gz ../data/
+tabix -p vcf ../data/00-All.vcf.gz
+
+# Finish and delete cruft
+rm -fr ../src
+rm -fr .
 
 echo Done
