f64ab15f594c503750ab42bcbecabb4d1d00a5b2
James Boocock <sfk2001@gmail.com>
2012-01-16 15:27:45 +1300

    still fixing install script errors

--- scripts/move_files.sh
+++ scripts/move_files.sh
@@ -26,31 +26,31 @@ cp -f universe_wsgi.webapp.ini /home/galaxy/galaxy-dist/
 cp -f tool_conf.xml /home/galaxy/galaxy-dist/
 
 # Setup snpEff
-mkdir /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
-cp -f ../src/snpEff/SnpSift.jar /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
-cp -f ../src/snpEff/snpEff.jar /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
-cp -f ../src/snpEff/snpEff.config /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
-cp -f ../src/snpEff/data /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
-mkdir /home/galaxy/galaxy-dist/tools/snpEff
-cp -f ../src/snpEff/snpEff.xml /home/galaxy/galaxy-dist/tools/snpEff/
+sudo mkdir /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
+sudo cp -f ../src/snpEff/SnpSift.jar /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
+sudo cp -f ../src/snpEff/snpEff.jar /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
+sudo cp -f ../src/snpEff/snpEff.config /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
+sudo cp -f ../src/snpEff/data /home/galaxy/galaxy-dist/tool-data/shared/jars/snpEff
+sudo mkdir /home/galaxy/galaxy-dist/tools/snpEff
+sudo cp -f ../src/snpEff/snpEff.xml /home/galaxy/galaxy-dist/tools/snpEff/
 
 # Setup VcfTools
-cp -f ../src/vcfperltools /home/galaxy/galaxy-dist/tool-data/shared/
+sudo cp -f ../src/vcfperltools /home/galaxy/galaxy-dist/tool-data/shared/
 
 # Setup GATK
-mkdir /home/galaxy/galaxy-dist/tool-data/shared/jars/gatk
+sudo mkdir /home/galaxy/galaxy-dist/tool-data/shared/jars/gatk
 cp -f ../src/gatk/GenomeAnalysisTK.jar /home/galaxy/galaxy-dist/tool-data/shared/jars/
-cp -f ../src/gatk /home/galaxy/galaxy-dist/tools/
+sudo cp -f ../src/gatk /home/galaxy/galaxy-dist/tools/
 
 # Shift all the tools
-cp -fR ../galaxy/ /home/galaxy/galaxy-dist/tools/SOER1000genes/
+sudo cp -fR ../galaxy/ /home/galaxy/galaxy-dist/tools/SOER1000genes/
 
 # Install dbSNP
 echo "Downloading dbSNP135 (~9Gb).. This may take some time.."
 echo "If you already have dbSNP and its tabix file please put them in /home/galaxy/galaxy-dist/tools/SOER1000genes/data/ folder named 00-All.vcf.gz and 00-All.vcf.gz.tbi accordingly and exit the script."
-wget ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/v4.0/00-All.vcf.gz 
-mkdir /home/galaxy/galaxy-dist/tools/SOER1000genes/data
-mv -f 00-All.vcf.gz /home/galaxy/galaxy-dist/tools/SOER1000genes/data/
+sudo wget ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/v4.0/00-All.vcf.gz 
+sudo mkdir /home/galaxy/galaxy-dist/tools/SOER1000genes/data
+sudo mv -f 00-All.vcf.gz /home/galaxy/galaxy-dist/tools/SOER1000genes/data/
 tabix -p vcf /home/galaxy/galaxy-dist/tools/SOER1000genes/data/00-All.vcf.gz
 
 echo Done!
