f60716a1bf38dcaed60b343cd3db81f11074bff2
James Boocock <sfk2001@gmail.com>
2012-01-19 18:40:24 +1300

    uoeuaoe

--- galaxy/ensemblVEP/ensembl.sh
+++ galaxy/ensemblVEP/ensembl.sh
@@ -1,2 +1,2 @@
 INPUTS=`echo $2 | tr "," " "`
-/home/galaxy/galaxy-dist/tools/SOER1000genes/galaxy/ensemblVEP/./ensembl_run.sh $1 $INPUTS
+~/galaxy-dist/tools/SOER1000genes/galaxy/ensemblVEP/./ensembl_run.sh $1 $INPUTS
