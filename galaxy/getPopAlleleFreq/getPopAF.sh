6cb3f31e9d0d6d5be3e909b9939c0f79f9ff72bb
Edward Hills <ehills666@gmail.com>
2012-01-30 10:47:40 +1300

    somethin

--- galaxy/getPopAlleleFreq/getPopAF.sh
+++ galaxy/getPopAlleleFreq/getPopAF.sh
@@ -18,5 +18,5 @@ REGION=`echo ${1} | awk -F[:] '{print $2}'`
 
 tabix -fh /home/galaxy/galaxy-dist/tools/SOER1000genes/data/1kg/vcf/ALL.wgs.phase1_intergrated_calls.20101123.snps_indels_svs.sites.vcf.gz ${CHROM}:${REGION} > $2
 
-java -jar GetPopAllele.jar $2 > $3
+java -jar /home/galaxy/galaxy-dist/tools/SOER1000genes/galaxy/getPopAlleleFreq/GetPopAllele.jar $2 > $3
 
