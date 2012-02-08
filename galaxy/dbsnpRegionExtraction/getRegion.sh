904b627c6bd831051baf1fed39ba19d34bc8045f
Edward Hills <ehills666@gmail.com>
2012-02-08 18:15:31 +1300

    made it so it can take a file with lots of chromosomes

--- galaxy/dbsnpRegionExtraction/getRegion.sh
+++ galaxy/dbsnpRegionExtraction/getRegion.sh
@@ -2,9 +2,14 @@
 # Author: Edward Hills
 # Date: 29/11/11
 #
-# This script will find the first line that does not contain a # comment
-# and echo out the chromosome followed by a : followed by a - separated
-# pair of regions. ie chromosome:startRegion-endRegion.
+# GetRegion will return all start and end positions for each chromosome
+# in the file given. The file must be sorted by chromosome as it will
+# not be able to match regions which have chromosomes not in order.
+# It will return each region in the format below
+# 
+#   chr#:startPos..endPos
+# 
+# for each list of chromosomes.
 #
 # Example use is cat inputFile.vcf | ./getRegion inputFile.vcf
 #
@@ -12,17 +17,49 @@
 # $1 = input_file
 
 awk '
-    {if ($1 !~ /'#'/)
-        {print $1":"$2}
+    {if ($1 ~ /'#'/)
+        {print $1}
     }
-    ' $1 > ~tmp.tmp
+    ' $1 > ~head.tmp
 
-FIRST_LINE=`head -1 ~tmp.tmp`
+count=1
+curr_chr=""
+curr_pos=""
+prev_chr=""
+prev_pos=""
+firstReg=""
 
-tail -1 $1 | awk '{print $2}' >> ~tmp.tmp
+while read line 
+do
+    curr_chr=`echo $line | awk '{if ($1 !~ /'#'/)
+                                    {print $1}
+                               }'`
 
-SECOND_LINE=`tail -1 ~tmp.tmp`
+    curr_pos=`echo $line | awk '{if ($1 !~ /'#'/)
+                                    {print $2}
+                               }'`
 
-echo $FIRST_LINE".."$SECOND_LINE
 
-rm -f ~tmp.tmp
+    if [ $count != 1 -a "$curr_chr" != "$prev_chr" ]
+    then
+        echo $firstReg..$prev_pos >> ~reg.tmp
+        count=1
+    fi
+
+    if [ $count == 1 ]
+    then
+        firstReg=$curr_chr:$curr_pos
+    fi
+   
+    prev_chr=$curr_chr
+    prev_pos=$curr_pos
+
+    if [ "$curr_chr" != "" ]
+    then
+        count=$((count + 1))
+    fi
+
+done < $1
+    
+# print final line as it wont be printed above
+echo $firstReg..$prev_pos >> ~reg.tmp
