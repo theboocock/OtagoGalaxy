3f7abc7cc860521b396ef850a10c17d05ddd740b
Galaxy <ehills666@gmail.com>
2012-03-28 19:03:32 +1300

    made it so it properly checks if AF fields are present and returns error if they are not

--- src/getAlleleFreqSummary/GetAlleleFreqSummary.java
+++ src/getAlleleFreqSummary/GetAlleleFreqSummary.java
@@ -1,7 +1,7 @@
 /**
  *
  * This converts a VCF containing Allele Frequency information 
- * and returns a user chosen delimitted file containing only column 
+ * and returns a chosen delimitted file containing only column 
  * headings containing 'AF'.
  *
  * @Author James Boocock and Edward Hills
@@ -15,31 +15,31 @@ import java.io.*;
 import java.util.regex.*;
 
 public class GetAlleleFreqSummary{
-	
-	private static ArrayList<String> infoColumnA;
-	private static int offSet;
-    private static final String DELIMITTER = "\t";
 
-	public static void main(String[] args){
-		if(args.length < 1 || args[0].equals("-h")){
-			usage();
-			System.exit(1);
-		}
-		try {
-		Scanner fileReader=new Scanner(new File(args[0]));
-		printKey();
-        buildColumnHeaders(fileReader);
-        System.out.println();
-		extractLine(fileReader);
-		} catch(FileNotFoundException e){
-		e.printStackTrace();
-		System.exit(1);
-		
-		}
-	}
+    private static ArrayList<String> infoColumnA;
+    private static int offSet;
+    private static final String DELIMITTER = "\t";
+    private static int howManyFound = 0;
+
+    public static void main(String[] args){
+        if(args.length < 1 || args[0].equals("-h")){
+            usage();
+            System.exit(1);
+        }
+        try {
+            Scanner fileReader=new Scanner(new File(args[0]));
+            buildColumnHeaders(fileReader);
+            System.out.println();
+            extractLine(fileReader);
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+            System.exit(1);
+
+        }
+    }
 
     public static void printKey() {
-        
+
         System.out.println("************* KEY *************************\n");
         System.out.println("LDAF \t = \t Linkage Disequilibrium Allele Frequency");
         System.out.println("AF \t = \t Allele Frequency");
@@ -51,92 +51,98 @@ public class GetAlleleFreqSummary{
 
     }
 
-	public static void usage(){
-		System.out.println("Program: Returns Allele Frequency formatted output\n");
-		System.out.println("Usage: java GetAlleleFreqSummary <IN.vcf>");
-	}
+    public static void usage(){
+        System.out.println("Program: Returns Allele Frequency formatted output\n");
+        System.out.println("Usage: java GetAlleleFreqSummary <IN.vcf>");
+    }
 
-	public static void extractLine(Scanner input){
-		String row="";
-		while(input.hasNextLine()){
-			row ="";
-			String line=input.nextLine();
-			Scanner tokens=new Scanner(line);
-			String[] listInfo=new String[infoColumnA.size()];
-			for(int i = 0; i < listInfo.length; i++){
-				listInfo[i] = "";
-			}
-			int x = 0;
-			while(x < 7){
-				x++;
-				row += tokens.next() + DELIMITTER;
-			}
-			if(tokens.hasNext()){	
-				String data=tokens.next();
-				String[] info = data.split(";");
-				for(int i = 0; i < info.length; i++){
-					String indValue= info[i];
-					String[] bothParts = indValue.split("=");
-					if(infoColumnA.contains(bothParts[0])){
-						int headerIndex = infoColumnA.indexOf(bothParts[0]);
-						if(bothParts[0].matches("^.*AF.*")){
-							listInfo[headerIndex] = bothParts[1];						
-						}
-					
-					}						
-				}
-				for(int i = 0; i < listInfo.length; i ++){
-					row+= listInfo[i] + DELIMITTER;	
-				}
-			}
-			while(tokens.hasNext()){
-				row+= tokens.next() + DELIMITTER;
-			}
-			System.out.println(row);
-		}
-	}
+    public static void extractLine(Scanner input){
+        String row="";
+        while(input.hasNextLine()){
+            row ="";
+            String line=input.nextLine();
+            Scanner tokens=new Scanner(line);
+            String[] listInfo=new String[infoColumnA.size()];
+            for(int i = 0; i < listInfo.length; i++){
+                listInfo[i] = "";
+            }
+            int x = 0;
+            while(x < 7){
+                x++;
+                row += tokens.next() + DELIMITTER;
+            }
+            if(tokens.hasNext()){	
+                String data=tokens.next();
+                String[] info = data.split(";");
+                for(int i = 0; i < info.length; i++){
+                    String indValue= info[i];
+                    String[] bothParts = indValue.split("=");
+                    if(infoColumnA.contains(bothParts[0])){
+                        int headerIndex = infoColumnA.indexOf(bothParts[0]);
+                        if(bothParts[0].matches("^.*_AF") || bothParts[0].matches("^AF")){
+                            listInfo[headerIndex] = bothParts[1];						
+                        }
 
-	public static void buildColumnHeaders(Scanner input){
-		String infoColumn="";
-		infoColumnA = new ArrayList<String>();
-		int lineCount=0;
-		while(input.hasNextLine()){
-			String line=input.nextLine();
-			if (line.matches("^##INFO.*")){
-					String columnName="";
-					int i = 11;
-					while(line.charAt(i) != ','){
-						columnName += line.charAt(i);	
-						i++;
-						}
-					if(columnName.matches("^.*AF.*")){
-					infoColumnA.add(columnName);
-					}
-				}
-			if (line.matches("^#CHROM.*")){
-				Scanner headerScan= new Scanner(line);
-				int countC = 0;
-				while(headerScan.hasNext()){
-					String columnName=headerScan.next();
-					if(countC == 0){
-						columnName = columnName.substring(1);
-					}
-					if(columnName.matches("INFO")){
-						offSet=countC;
-						for(String temp: infoColumnA){
-							infoColumn += temp + DELIMITTER;
-						}
-					}else{
-						infoColumn += columnName + DELIMITTER;
-					}
-					countC++;	
-					
-				}
-			System.out.println(infoColumn);
-			return;
-			}
-			lineCount++;
-		}
-	}
+                    }						
+                }
+                for(int i = 0; i < listInfo.length; i ++){
+                    row+= listInfo[i] + DELIMITTER;	
+                }
+            }
+            while(tokens.hasNext()){
+                row+= tokens.next() + DELIMITTER;
+            }
+            System.out.println(row);
+        }
+    }
+
+    public static void buildColumnHeaders(Scanner input){
+        String infoColumn="";
+        infoColumnA = new ArrayList<String>();
+        int lineCount=0;
+        while(input.hasNextLine()){
+            String line=input.nextLine();
+            if (line.matches("^##INFO.*")){
+                String columnName="";
+                int i = 11;
+                while(line.charAt(i) != ','){
+                    columnName += line.charAt(i);	
+                    i++;
+                }
+                if(columnName.matches("^.*_AF") || columnName.matches("^AF")){
+                    infoColumnA.add(columnName);
+                    howManyFound++;
+                }
+            }
+            if (line.matches("^#CHROM.*")){
+                Scanner headerScan= new Scanner(line);
+                int countC = 0;
+                while(headerScan.hasNext()){
+                    String columnName=headerScan.next();
+                    if(countC == 0){
+                        columnName = columnName.substring(1);
+                    }
+                    if(columnName.matches("INFO")){
+                        offSet=countC;
+                        for(String temp : infoColumnA){
+                            infoColumn += temp + DELIMITTER;
+                        }
+                    }else{
+                        infoColumn += columnName + DELIMITTER;
+                    }
+                    countC++;	
+
+                }
+                if (howManyFound == 0) {
+                    System.err.println("Sorry no fields matching AF or <pop>_AF were found.");
+                    System.exit(1);
+                }
+                printKey();
+                System.out.println(infoColumn);
+                return;
+            }
+            lineCount++;
+        }
+    }
 
 }
