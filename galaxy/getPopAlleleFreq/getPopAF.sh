6890ec2d4ae6f8bb85cff2f98b04c120ff40e7c0
Galaxy <ehills666@gmail.com>
2012-04-19 17:06:14 +1200

    changed filename

--- galaxy/getPopAlleleFreq/getPopAF.sh
+++ galaxy/getPopAlleleFreq/getPopAF.sh
@@ -16,7 +16,7 @@
 CHROM=`echo ${1} | awk -F[:] '{print $1}'`
 REGION=`echo ${1} | awk -F[:] '{print $2}'`
 
-tabix -fh /home/galaxy/galaxy-dist/tools/SOER1000genes/data/1kg/vcf/ALL.wgs.phase1_integrated_calls.*.snps_indels_svs.sites.vcf.gz ${CHROM}:${REGION} > $2
+tabix -fh /home/galaxy/galaxy-dist/tools/SOER1000genes/data/1kg/vcf/ALL.wgs.phase1_release_v2*.gz $1 > $2
 
 java -jar /home/galaxy/galaxy-dist/tool-data/shared/jars/alleleFreq/GetAlleleFreqSummary.jar $2 > $3
 
