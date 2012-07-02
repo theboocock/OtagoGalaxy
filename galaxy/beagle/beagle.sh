c8a26a96fb7f7ff8c0a893e5b1159387cf83898d
James Boocock <jamesboocock@MikBlack-Lab-2.(none)>
2012-07-02 16:55:56 +1200

    Have imputting working on beagle.xml

--- galaxy/beagle/beagle.sh
+++ galaxy/beagle/beagle.sh
@@ -6,10 +6,56 @@
 # $1 impute, ibd or assoctest $2 command line argument
 #
 PREFIX=`date '+%s'`
-COMMAND="${1} out=$PREFIX"
+GPROBS_FILE=''
+DOSE_FILE=''
+LOGFILE=''
+PHASED=''
+RSQUARED_FILE=''
+
+while getopts "l:c:p:g:d:r:" opt; do
+case $opt in
+c)
+COMMAND="${OPTARG} out=$PREFIX"
+;;
+l)
+LOGFILE=$OPTARG
+;;
+p)
+PHASED=$OPTARG
+;;
+g)
+GPROBS_FILE=$OPTARG
+;;
+d)
+DOSE_FILE=$OPTARG
+;;
+r)
+RSQUARED_FILE=$OPTARG
+;;
+esac
+done
+
+if [ "${COMMAND}" != "" ]; then
 eval $COMMAND > /dev/null
-mv $PREFIX.log $2
+fi
+
+if [ "${LOGFILE}" != "None" ]; then
+     mv $PREFIX.log $LOGFILE
+fi
+
+if [ "${PHASED}" != "None" ]; then
 gunzip $PREFIX.*.phased.gz
-mv $PREFIX.*.phased $3
+mv $PREFIX.*.phased $PHASED
+gunzip $PREFIX.*.gprobs.gz
+mv $PREFIX.*.gprobs $GPROBS_FILE
+gunzip $PREFIX.*.dose.gz
+mv $PREFIX.*.dose $DOSE_FILE
+mv $PREFIX.*.r2 $RSQUARED_FILE
+fi
+
+
+#mv $PREFIX.log $2
+#gunzip $PREFIX.*.phased.gz
+#mv $PREFIX.*.phased $3
 
 
