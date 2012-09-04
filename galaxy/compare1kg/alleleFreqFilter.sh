9e3b30e1cb151cdb9527f3bbb24d987ee2892dc8
ehills <edward.hills@otago.ac.nz>
2012-09-04 16:56:20 +1200

    fixed compare 1kg

--- galaxy/compare1kg/alleleFreqFilter.sh
+++ galaxy/compare1kg/alleleFreqFilter.sh
@@ -5,21 +5,49 @@
 #
 # $1 file
 # $2 threshold below which to throw away line
-
+# $3 more or less
+# $4 snps / indels / all
 
 while read line
 do
-    TEMP_LINE=`echo $line | awk '{if($8 ~ /AF=/){print $8}}'`
-    if [ "$TEMP_LINE" != "" ] ;then
-        AF_CHECK=`echo $TEMP_LINE | awk -F[\;] '{ for (i=1;i<= NF;i++) {                if ($i ~/AF=/){print $i}}}'`
-        if ["$AF_CHECK" != "" ]; then
-            AF_NUMBER=`echo $AF_CHECK | awk -v AF=$2 -F[\=] '{if($2 >= AF){print{$0}}}'`
-            if ["$AF_NUMBER" != 0 ];then
-                    echo $line
-                fi
-            fi
-    fi
-done < $1
 
-    
+    if [ "$4" != "all" ] ;then
+        line=`echo $line | grep -i "$4"`
+        if [ "$line" == "" ] ;then
+            continue
+        fi
+    fi
+
+    # get the info column
+    INFO_COLUMN=`echo $line | awk '{if($8 ~ /AF=/){print $8}}'`
+    if [ "$INFO_COLUMN" != "" ] ;then
+
+        #get the af=freq pair
+        AF_PAIR=`echo $INFO_COLUMN | awk -F[\;] '{ for (i=1;i<= NF;i++) {
+                                                            if ($i ~/AF=/)
+                                                                {print $i}
+                                                            }
+                                                }'`
+
+        # print the line if is more or less than threshold and snp/indel
+        if [ "$AF_PAIR" != "" ]; then
+
+            if [ "$3" == "more" ] ;then
+                AF_NUMBER=`echo $AF_PAIR | awk -v AF=$2 -F[\=] '{if($2 >= AF){
+                                                                    print $0
+                                                                }}'`
+            fi
+            if [ "$3" == "less" ] ;then
+                AF_NUMBER=`echo $AF_PAIR | awk -v AF=$2 -F[\=] '{if($2 <= AF){
+                                                                    print $0
+                                                                }}'`
+            fi
+
+            if [ "$AF_NUMBER" != "" ];then
+                    echo $line
+            fi
+        fi
+    fi
+
+done < $1
 
--- galaxy/compare1kg/compare1kg.sh
+++ galaxy/compare1kg/compare1kg.sh
@@ -5,11 +5,12 @@
 # $3 = value for grep in myvcf
 # $4 = <= || >=
 # $5 = value for grep in 1kg
-# $6 = SNPs or Indels (not yet implemented)
-#TODO ^^
+# $6 = SNPs or Indels
+# $7 = output vcf
 
 ONEKG_VCF=`ls ~/galaxy-dist/tools/SOER1000genes/data/1kg/vcf/ALL.*.gz`
 
+# Print out header into new file
 while read line
     do
         HEADER=`echo $line | awk '{if ($1 ~ /^#/) {print $0}}'`
@@ -18,41 +19,37 @@ while read line
             break
         fi
 
-        echo $HEADER >> header.txt
+        echo $HEADER >> $7
 
-    done < $1
+done < $1
 
 # Filter original
-while read line
-    do
-        echo $line | grep "AF=${3}" >> filtered_myVCF.vcf
-    done < $1
 
+~/galaxy-dist/tools/SOER1000genes/galaxy/compare1kg/./alleleFreqFilter.sh $1 $3 $2 $6 >| filtered_myVCF.vcf
 
 # get positions from filtered original
 awk '{print $1":"$2"-"$2}' < filtered_myVCF.vcf >| pos1.txt
 
+bgzip filtered_myVCF.vcf
+tabix -fp vcf filtered_myVCF.vcf.gz
+
 # gets position from my filtered vcf
 while read line
     do
-        tabix -f $ONEKG_VCF "$line" >> filtered.1kg.vcf
+        tabix -f $ONEKG_VCF $line >> filtered.1kg.vcf
     done < pos1.txt
 
-
 #filter 1kg vcf
-cat filtered.1kg.vcf | grep "AF=${5}" > final_filtered_1kg.vcf
- 
+~/galaxy-dist/tools/SOER1000genes/galaxy/compare1kg/./alleleFreqFilter.sh filtered.1kg.vcf $5 $4 $6 >| final_filtered_1kg.vcf
 
 awk '{print $1":"$2"-"$2}' < final_filtered_1kg.vcf >| pos2.txt
 
-bgzip -c > filtered_myVCF.vcf
-tabix -p vcf filtered_myVCF.vcf.gz
-
+# print out all those that match the 1kg and myvcf
 while read line
     do
-        tabix -f filtered_myVCF.vcf "$line" >> header.txt
+        #TODO fix this
+        tabix -f filtered_myVCF.vcf.gz $line >> $7
     done < pos2.txt
 
-mv header.txt final_filtered.vcf
-
 exit 0
+
