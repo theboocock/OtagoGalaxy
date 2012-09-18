2de822c0482bc31fd34984a057ddb2e3b84ef27f
Edward Hills <ehills666@gmail.com>
2012-09-18 16:25:48 +1200

    fixed it up a bit

--- galaxy/impute2/impute2.sh
+++ galaxy/impute2/impute2.sh
@@ -3,13 +3,25 @@
 # $1 = start pos
 # $2 = end pos
 # $3 = chr number of file
-# $4 = file of known haps (mydata.haps)
+# $4 = file of known haps (mydata.hap)
 # $5 = geno file
 # $6 = summary file
 # $7 = warnings file
 # $8 = info file
+# $9 = phased or not
+# $10 = Ne
 
 java GenerateImputePairs $1 $2 > ~generated.tmp
+
+if [ "$9" == "unphased" ] ; then
+
+    PHASE="-g"
+
+else
+
+    PHASE="-known_haps_g"
+
+fi
 
 while read line
 do
@@ -21,7 +33,7 @@ do
     -m ~/galaxy-dist/tools/SOER1000genes/data/1kg/impute2/ALL_1000G_phase1integrated_v3_impute/genetic_map_chr${3}_combined_b37.txt \ 
     -h ~/galaxy-dist/tools/SOER1000genes/data/1kg/impute2/ALL_1000G_phase1integrated_v3_impute/ALL_1000G_phase1integrated_v3_chr${3}_impute.hap \ 
     -l ~/galaxy-dist/tools/SOER1000genes/data/1kg/impute2/ALL_1000G_phase1integrated_v3_impute/ALL_1000G_phase1integrated_v3_chr${3}_impute.legend \ 
-    -known_haps_g $4 -int ${START}e6 ${END}e6 -Ne 20000 -buffer 250 -o $5 \ 
+    $PHASE $4 -int ${START}e6 ${END}e6 -Ne ${10} -buffer 250 -o $5 \ 
     -r $6 -w $7 -i $8 -os 0 1 2 3
 
 done < ~generated.tmp
