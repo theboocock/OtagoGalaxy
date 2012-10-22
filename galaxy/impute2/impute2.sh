5e3f661e57e45164d3c1c270f51f6fcba105df81
Edward Hills <ehills666@gmail.com>
2012-10-23 12:18:33 +1300

    fixed up impute2 i think, cant test atm

--- galaxy/impute2/impute2.sh
+++ galaxy/impute2/impute2.sh
@@ -10,6 +10,7 @@
 # $8 = info file
 # $9 = phased or not
 # $10 = Ne
+# $11 = buffer_size
 
 java GenerateImputePairs $1 $2 > ~generated.tmp
 
@@ -33,7 +34,7 @@ do
     -m ~/galaxy-dist/tools/SOER1000genes/data/1kg/impute2/ALL_1000G_phase1integrated_v3_impute/genetic_map_chr${3}_combined_b37.txt \ 
     -h ~/galaxy-dist/tools/SOER1000genes/data/1kg/impute2/ALL_1000G_phase1integrated_v3_impute/ALL_1000G_phase1integrated_v3_chr${3}_impute.hap \ 
     -l ~/galaxy-dist/tools/SOER1000genes/data/1kg/impute2/ALL_1000G_phase1integrated_v3_impute/ALL_1000G_phase1integrated_v3_chr${3}_impute.legend \ 
-    $PHASE $4 -int ${START}e6 ${END}e6 -Ne ${10} -buffer 250 -o $5 \ 
+    $PHASE $4 -int ${START}e6 ${END}e6 -Ne ${10} -buffer ${11} -o $5 \ 
     -r $6 -w $7 -i $8 -os 0 1 2 3
 
 done < ~generated.tmp
