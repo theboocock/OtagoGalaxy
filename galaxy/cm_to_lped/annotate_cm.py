41c1a82a0bc277be168d75d950260f727b21666d
ehills <edward.hills@otago.ac.nz>
2012-11-16 17:10:15 +1300

    yes done for now

--- galaxy/cm_to_lped/annotate_cm.py
+++ galaxy/cm_to_lped/annotate_cm.py
@@ -14,25 +14,25 @@ import sys
 def main():
     genetic_map_file=sys.argv[1]
     user_map_file=sys.argv[2]
-        with open(file, 'r') as umap:
-            with open(file, 'r') as gmap:
-                u_line=read.line()
-                g_line=read.line()
-                    while(gmap and umap):
-                        u_line=u_line.split()
-                        g_line=g_line.split()
-                        u_posl=u_line[3]
-                        g_line=g_line[0]
-                        if(u_posl == g_pos1):
-                            print u_line[0] + ' '+ u_line[1] + ' ' + g_line[2]+ ' ' + u_line[3]
-                        elif (u_posl < g_pos1):
-                            u_line=read.line()
-                            if not u_line: break
-                        else:
-                            g_line=read.line()
-                            if not g_line: break
-                        u_line=read.line()
-                        g_line=read.line()
+    with open(user_map_file, 'r') as umap:
+        with open(genetic_map_file, 'r') as gmap:
+            u_line=umap.readline()
+            g_line=gmap.readline()
+            while(gmap and umap):
+                u_line=u_line.split()
+                g_line=g_line.split()
+                u_posl=u_line[3]
+                g_line=g_line[0]
+                if(u_posl == g_pos1):
+                    print u_line[0] + ' '+ u_line[1] + ' ' + g_line[2]+ ' ' + u_line[3]
+                elif (u_posl < g_pos1):
+                    u_line=umap.readline()
+                if not u_line: break
+                else:
+                    g_line=read.line()
+                    if not g_line: break
+                u_line=umap.readline()
+                g_line=gmap.readline()
 
 
             
--- galaxy/cm_to_lped/cm_to_lped.sh
+++ galaxy/cm_to_lped/cm_to_lped.sh
@@ -6,19 +6,21 @@
 # $1 extrafiles path
 # $2 basename
 # $3 galaxy impute data folder containing the centimorgan files
+# $4 galaxy root dir
 #
-#
 
-cp $2 $1
+cp $1/$2.ped $2.ped 
 
+cat $1/$2.map | awk '{print $1}' | uniq > chromosomes
 
-cat $1| awk '{print $1}' | uniq > chromosomes
 
 while read line
 do
     CHROM=$line
-    MAP_FILE=${3}genetic_map_chr${line}_combined_b37.txt 
-    python annotate_cm.py $MAP_FILE "${1}.${2}.map" >> out.tmp
+    MAP_FILE=${3}/genetic_map_chr${line}_combined_b37.txt 
+    echo $MAP_FILE    
+    python $4/tools/SOER1000genes/galaxy/cm_to_lped/annotate_cm.py $MAP_FILE "${1}/${2}.map" >> out.tmp
 done < chromosomes
+
 cp out.tmp $1$2
 
