4ba5c681ca32981eb5c701093959940d6497d9d0
ehills <edward.hills@otago.ac.nz>
2012-11-19 06:25:23 +1300

    slowest script ever

--- galaxy/cm_to_lped/annotate_cm.py
+++ galaxy/cm_to_lped/annotate_cm.py
@@ -3,6 +3,7 @@
 #
 # $1 GENETIC_MAP FILE
 # $2 user_MAP FILE
+# $3 chrom
 #
 # @author James Boocock
 # @date   16/11/2012
@@ -14,26 +15,31 @@ import sys
 def main():
     genetic_map_file=sys.argv[1]
     user_map_file=sys.argv[2]
+    if (sys.argv[3] == 'X'):
+        sys.arv[3] = '24'
     with open(user_map_file, 'r') as umap:
         with open(genetic_map_file, 'r') as gmap:
-            u_line=umap.readline()
-            g_line=gmap.readline()
-            while(gmap and umap):
-                u_line=u_line.split()
-                g_line=g_line.split()
-                u_posl=u_line[3]
-                g_line=g_line[0]
-                if(u_posl == g_pos1):
-                    print u_line[0] + ' '+ u_line[1] + ' ' + g_line[2]+ ' ' + u_line[3]
-                elif (u_posl < g_pos1):
-                    u_line=umap.readline()
-                if not u_line: break
-                else:
-                    g_line=read.line()
-                    if not g_line: break
-                u_line=umap.readline()
-                g_line=gmap.readline()
+            u_line_temp=umap.readline()
+            g_line_temp=gmap.readline()
+            if (g_line_temp.split()[0] == "position"):
+                g_line_temp=gmap.readline()
+            while(g_line_temp and u_line_temp):
 
+                u_line=u_line_temp.split()
+                g_line=g_line_temp.split()
+                u_pos1=u_line[3]
+                g_pos1=g_line[0]
+                if (u_line[0] == sys.argv[3]):
+                    if(long(u_pos1) == long(g_pos1)):
+                        print "WIN2 " +  u_line[0] + ' '+ u_line[1] + ' ' + g_line[2]+ ' ' + u_line[3]
+                        u_line_temp=umap.readline()
+                        g_line_temp=gmap.readline()
+                    elif (long(u_pos1) < long(g_pos1)):
+                        u_line_temp=umap.readline()
+                    else:
+                        g_line_temp=gmap.readline()
+                else:
+                    u_line_temp=umap.readline()
 
             
 
--- galaxy/cm_to_lped/cm_to_lped.sh
+++ galaxy/cm_to_lped/cm_to_lped.sh
@@ -10,17 +10,23 @@
 #
 
 cp $1/$2.ped $2.ped 
-
 cat $1/$2.map | awk '{print $1}' | uniq > chromosomes
 
 
 while read line
 do
+    
+    if [ $line == "23" ]; then
+        line="X_PAR2"
+    fi
     CHROM=$line
-    MAP_FILE=${3}/genetic_map_chr${line}_combined_b37.txt 
-    echo $MAP_FILE    
-    python $4/tools/SOER1000genes/galaxy/cm_to_lped/annotate_cm.py $MAP_FILE "${1}/${2}.map" >> out.tmp
+    MAP_FILE=${3}genetic_map_chr${line}_combined_b37.txt
+    echo "${1}/${2}.map"
+    echo $MAP_FILE
+    if [ ${line} != "26" ]  && [ ${line} != "24" ]; then
+         python $4/tools/SOER1000genes/galaxy/cm_to_lped/annotate_cm.py $MAP_FILE "${1}/${2}.map" >> out.tmp
+    fi
 done < chromosomes
-
-cp out.tmp $1$2
+cp -f out.tmp "${2}.map"
+echo "${2}.map"
 
