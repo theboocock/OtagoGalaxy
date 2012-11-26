09282ad2988a18cfb0c78248dbdc6dc942634276
James Boocock <sfk2001@gmail.com>
2012-11-26 15:17:19 +1300

    added file name

--- galaxy/vcfSubset/select_samples.sh
+++ galaxy/vcfSubset/select_samples.sh
@@ -6,24 +6,14 @@
 # Filter by sampleId. Will separate the Sample specific data from
 # input VCF and put them into a new VCF
 #
-# $1..($#-1) = sample ids
-# $($#) = output file
-# $($#) = output file
+# $1 = sample ids
+# $2 = input file
+# $3 = output file
+#
 
-SAMPLE_LIST=""
-NUM_SAMPLES=$#
-NUM_SAMPLES=$((NUM_SAMPLES - 2))
-
-for ((i=1; i<=NUM_SAMPLES; i++))
-do
-    INPUT=$1
-    SAMPLE_LIST="${SAMPLE_LIST}$INPUT,"
-    shift 
-done
-
-INPUT_FILENAME=$1
-shift
-OUTPUT_FILENAME=$1
+SAMPLE_LIST=`cat $1`
+INPUT_FILENAME=$2
+OUTPUT_FILENAME=$3
 bgzip -c $INPUT_FILENAME > INPUT.gz
 tabix -p vcf INPUT.gz
 
