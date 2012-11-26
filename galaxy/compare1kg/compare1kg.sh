5c692a4b251b630315eb3d7f281884fcf203b010
ehills <edward.hills@otago.ac.nz>
2012-11-26 22:44:04 +1300

    fixed

--- galaxy/compare1kg/compare1kg.sh
+++ galaxy/compare1kg/compare1kg.sh
@@ -34,13 +34,16 @@ done < $1
 # Filter original
 
 ~/galaxy-dist/tools/SOER1000genes/galaxy/compare1kg/./alleleFreqFilter.sh $1 $3 $2 $6 $8 $9 >| filtered_myVCF.vcf
-
 # get positions from filtered original
 awk '{print $1":"$2"-"$2}' < filtered_myVCF.vcf >| pos1.txt
 
 bgzip filtered_myVCF.vcf
 tabix -fp vcf filtered_myVCF.vcf.gz
 
+if [ ! -f pos1.txt ]; then
+    echo "No snps returned on first operation" >& 2
+    exit 1
+fi
 # gets position from my filtered vcf
 while read line
     do
