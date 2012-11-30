f831db28ada6a14ab83d532caa25398ebba1090d
Edward Hills <ehills666@gmail.com>
2012-11-30 14:09:41 +1300

    more scripting, more converters\!

--- scripts/create_converter_symlinks.sh
+++ scripts/create_converter_symlinks.sh
@@ -10,4 +10,13 @@ ln -fs ~/galaxy-dist/tools/SOER1000genes/galaxy/beagle/vcf_to_beagle.xml ~/galax
 ln -fs ~/galaxy-dist/tools/SOER1000genes/galaxy/beagle/vcf_to_beagle.sh ~/galaxy-dist/lib/galaxy/datatypes/converters/
 ln -fs ~/galaxy-dist/tools/SOER1000genes/galaxy/vcf_to_ped/vcf_to_ped.xml ~/galaxy-dist/lib/galaxy/datatypes/converters/
 
+echo "lped converters.."
+
+ln -fs ~/galaxy-dist/tools/SOER1000genes/galaxy/plink/ped_to_tped.xml ~/galaxy-dist/lib/galaxy/datatypes/converters/
+ln -fs ~/galaxy-dist/tools/SOER1000genes/galaxy/beagle/ped_to_beagle.xml ~/galaxy-dist/lib/galaxy/datatypes/converters/
+
+echo "tped converters.."
+
+ln -fs ~/galaxy-dist/tools/SOER1000genes/galaxy/tped_to_lped/tped_to_lped.xml ~/galaxy-dist/lib/galaxy/datatypes/converters/
+
 echo -e "\nDone!"
