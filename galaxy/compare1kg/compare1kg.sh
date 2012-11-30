c2fda4773a8afe45da2945454ca98da909ec3501
Edward Hills <ehills666@gmail.com>
2012-11-30 15:43:48 +1300

    stufsf

--- galaxy/compare1kg/compare1kg.sh
+++ galaxy/compare1kg/compare1kg.sh
@@ -3,13 +3,13 @@
 # FIXME: All references of 1kg are no longer true as we now use evs too.
 # $1 = my vcf
 # $2 = <= || >=
-# $3 = value for grep in myvcf
+# $3 = af value 1
 # $4 = <= || >=
-# $5 = value for grep in 1kg
-# $6 = SNPs or Indels
+# $5 = af value 2 (for 1kg)
+# $6 = search SNPs or Indels or both
 # $7 = output vcf
 # $8 = database
-# $9 = population (optional)
+# $9 = population - if database == evs
 
 if [ "$8" == "1kg" ] 
 then
@@ -19,21 +19,12 @@ else
 fi
 
 # Print out header into new file
-while read line
-    do
-        HEADER=`echo $line | awk '{if ($1 ~ /^#/) {print $0}}'`
-        if [ "$HEADER" == "" ]
-        then
-            break
-        fi
-
-        echo $HEADER >> $7
-
-done < $1
+cat $1 | awk '{if ($1 ~ /^#/) {print $0}}' > $7
 
 # Filter original
 
 ~/galaxy-dist/tools/SOER1000genes/galaxy/compare1kg/./alleleFreqFilter.sh $1 $3 $2 $6 $8 $9 >| filtered_myVCF.vcf
+
 # get positions from filtered original
 awk '{print $1":"$2"-"$2}' < filtered_myVCF.vcf >| pos1.txt
 
