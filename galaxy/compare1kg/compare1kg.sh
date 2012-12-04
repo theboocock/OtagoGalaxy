d4c41c47ee3cbb19d36949f64e468e0a757e8c89
ehills <edward.hills@otago.ac.nz>
2012-12-04 18:21:53 +1300

    fixed merge

--- galaxy/compare1kg/compare1kg.sh
+++ galaxy/compare1kg/compare1kg.sh
@@ -4,7 +4,6 @@
 #
 # FIXME: All references of 1kg are no longer true as we now use evs too.
 # $1 = my vcf
-<<<<<<< HEAD
 # $2 = less or more
 # $3 = af for 'myvcf'
 # $4 = less or more
@@ -13,16 +12,6 @@
 # $7 = output vcf
 # $8 = database
 # $9 = population if database == evs
-=======
-# $2 = <= || >=
-# $3 = af value 1
-# $4 = <= || >=
-# $5 = af value 2 (for 1kg)
-# $6 = search SNPs or Indels or both
-# $7 = output vcf
-# $8 = database
-# $9 = population - if database == evs
->>>>>>> c2fda4773a8afe45da2945454ca98da909ec3501
 
 if [ "$8" == "1kg" ] 
 then
@@ -31,28 +20,15 @@ else
     DATABASE=`ls ~/galaxy-dist/tools/SOER1000genes/data/evs/esp/ESP6500.ALL.snps.vcf.gz`
 fi
 
-<<<<<<< HEAD
 # Filter original -fix to get positions only
 # eg grep "AF=0.[5-9]" my.vcf |awk '{print $1":"$2"-"$2}' > pos1.txt
-=======
 # Print out header into new file
 cat $1 | awk '{if ($1 ~ /^#/) {print $0}}' > $7
->>>>>>> c2fda4773a8afe45da2945454ca98da909ec3501
 
 java -jar ~/galaxy-dist/tool-data/shared/jars/AFfilter/AFfilter.jar $2 $3 $9 < $1 >| pos.txt
 
-<<<<<<< HEAD
 #remove potential duplicates before matching - assumes vcf is sorted
 uniq pos.txt > pos1.txt 
-=======
-~/galaxy-dist/tools/SOER1000genes/galaxy/compare1kg/./alleleFreqFilter.sh $1 $3 $2 $6 $8 $9 >| filtered_myVCF.vcf
-
-# get positions from filtered original
-awk '{print $1":"$2"-"$2}' < filtered_myVCF.vcf >| pos1.txt
-
-bgzip filtered_myVCF.vcf
-tabix -fp vcf filtered_myVCF.vcf.gz
->>>>>>> c2fda4773a8afe45da2945454ca98da909ec3501
 
 if [ ! -f pos1.txt ]; then
     echo "No snps returned on first operation" >& 2
