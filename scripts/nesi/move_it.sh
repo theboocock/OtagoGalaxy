0c9c6393f42b2056d993b7923b320930efd2cf6d
Edward Hills <ehills666@gmail.com>
2012-12-19 17:49:40 +1300

    shouldnt have delted galaxy dist

--- scripts/nesi/move_it.sh
+++ scripts/nesi/move_it.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mv -f submit_job.py ../../../lib/galaxy/jobs/runners/
-mv -f check_jobs.py ../../../lib/galaxy/jobs/runners/
-mv -f get_results.py ../../../lib/galaxy/jobs/runners/
-mv -f stop_job.py ../../../lib/galaxy/jobs/runners/
-mv -f nesi.py ../../../lib/galaxy/jobs/runners/
+mv -f submit_job.py ../../../../lib/galaxy/jobs/runners/
+mv -f check_jobs.py ../../../../lib/galaxy/jobs/runners/
+mv -f get_results.py ../../../../lib/galaxy/jobs/runners/
+mv -f stop_job.py ../../../../lib/galaxy/jobs/runners/
+mv -f nesi.py ../../../../lib/galaxy/jobs/runners/
