8cc17b1005208989b9fb3538d5d8124e8b546245
James Boocock <sfk2001@gmail.com>
2013-03-27 17:04:34 +1300

    Made changes for impute2 to work on the nesi cluster

--- src/composite_datatypes/create_composite_rgenetics.py
+++ src/composite_datatypes/create_composite_rgenetics.py
@@ -16,6 +16,34 @@ import html_creation
 #
 ""
 
+galhtmlprefix = """<?xml version="1.0" encoding="utf-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="generator" content="Galaxy %s tool output - see http://g2.trac.bx.psu.edu/" />
+<title></title>
+<link rel="stylesheet" href="/static/style/base.css" type="text/css" />
+</head>
+<body>
+<div class="document">
+"""
+
+galhtmlpostfix = """</div>\n</body>\n</html>\n"""
+
+def create_html(file_dir, html_file, base_name, title_page):
+    f = file(html_file, 'w')
+    f.write(galhtmlprefix)
+    f.write("<div>")
+    f.write(title_page)
+    f.write("</div>")
+    flist = os.listdir(file_dir)
+    for i, data in enumerate(flist):
+        f.write('<li><a href="%s">%s</a></li>\n' % (os.path.split(data)[-1],os.path.split(data)[-1]))
+    f.write(galhtmlpostfix)
+    f.close()
+
+
 rgenetics_title="## Rgenetics: http://rgenetics.org Galaxy Tools"
 gtool_title="## Gtool data, Otago Galaxy Tools"
 shapeit_title="## ShapeIt data, Otago Galaxy Tools"
