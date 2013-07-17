7cdefc3d0121ade1876609ebc99860835fafd91c
MurrayCadzow <murraycadzow@merrimanlab-PC.(none)>
2013-07-17 12:51:02 +1200

    doesn't write out col or row names now

--- SelectionScripts/haps_remove_indels.R
+++ SelectionScripts/haps_remove_indels.R
@@ -1,5 +1,4 @@
 args<-commandArgs(TRUE)
 #read in haps file from shapeit
 hapsPop=read.table(file=args[1])
-hapsPop=hapsPop[nchar(as.character(hapsPop[,4]))==1 & nchar(as.character(hapsPop[,5]))==1, ] #remove indels
-write.table(hapsPop, file=paste(args1,".mod",sep=""))
+write.table(hapsPop, file=paste(args1,".mod",sep="", quote=FALSE, row.names=FALSE, col.names=FALSE))
