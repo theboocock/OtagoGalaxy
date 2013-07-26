8c986b888bcbe564eb0013c703c6a6aafb788adf
MurrayCadzow <murraycadzow@merrimanlab-PC.(none)>
2013-07-26 13:10:13 +1200

    Rscript to combine the split large windows

---
+++ SelectionScripts/nesi_recombine_ihh.R
@@ -0,0 +1,55 @@
+args<-commandArgs(TRUE)
+pop=as.character(args[1])
+chr=as.numeric(args[2])
+window=as.numeric(args[3])
+smallWindowOverlap=as.numeric(args[4]))
+overlap=as.numeric(args[5])
+cores=as.numeric(args[6])
+
+
+
+##use for testing
+#window=5000000
+#overlap = 1000000
+#cores = 10
+bigWindow= (window-smallWindowOverlap) * cores
+
+setwd(workingdir)
+fileList=dir(pattern="*wd.*ihh", recursive=TRUE)
+ihh_list=list()
+for(i in 1:length(fileList)){ihh_list[[i]]<-read.table(fileList[i])}
+names(ihh_list)<-fileList
+fileNumber=1:length(ihh_list)
+
+for (n in fileNumber){
+  print((n -1)* (window-overlap))
+  print(paste("window",n,": is from:", bigWindow * (n-1), "to:", bigWindow * n + overlap , sep=" "))
+  print(paste("merge window",n,": is from:",bigWindow * (n-1) + 1/2 * overlap, "to:", bigWindow * n + 1/2 * overlap, sep=" "))
+  
+  if(n == 1){ # from start to first half of overlaped region (first chunk)
+    print("n=1")
+    results = ihh_list[[n]][ ihh_list[[n]][,2] <= (n * bigWindow - 1/2 *overlap) ,] #correct window
+    print(max(results[,2]))
+  } else {
+    if(n == max(fileNumber)){ #take second half of overlap at start and go until the end (final chunk)
+      print("max")
+      a= results
+      b = ihh_list[[n]][ ( bigWindow* (n-1) + 1/2*overlap) < ihh_list[[n]][,2]  ,]
+      print(max(results[,2]))
+      print(min(b[,2]))
+      results = rbind(a,b)
+      print(max(results[,2]))
+    } else { #start =take second half of overlap, end = take first half (middle regions)
+      print("middle")
+      
+      a = results
+      b = ihh_list[[n]][ ( bigWindow* (n-1) + 1/2*overlap) < ihh_list[[n]][,2]  & ihh_list[[n]][,2] <=  (bigWindow* n + (1/2 * overlap)), ]
+      print(max(a[,2]))
+      print(min(b[,2]))
+      results = rbind(a,b )
+      print(max(results[,2]))
+    }
+  } 
+}
+
+write.table(results, file=paste(pop,"_chr",chr,".iHH",sep=""),quote=FALSE, sep="\t")
