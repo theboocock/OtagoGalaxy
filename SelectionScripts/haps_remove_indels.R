720f8d92f00c60813dcc8bf9659218f52fe7738a
James Boocock <sfk2001@gmail.com>
2013-07-29 11:12:07 +1200

    Removed the selection Pipeline

--- SelectionScripts/haps_remove_indels.R
+++
@@ -1,4 +0,0 @@
-args<-commandArgs(TRUE)
-#read in haps file from shapeit
-hapsPop=read.table(file=args[1])
-write.table(hapsPop, file=paste(args1,".mod",sep="", quote=FALSE, row.names=FALSE, col.names=FALSE))
